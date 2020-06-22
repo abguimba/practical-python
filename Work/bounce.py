fe0426ce763cf134870adcd6feef9b3056953c57
Lincoln <abraham@sherpa-advisory.com>
2020-06-22 21:31:49 +0200

    Add solution files for section 1.2

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+
+def main():
+    height = 100
+    for bounce in range(1, 11):
+        height *= 0.6
+        print(f"{bounce} {str(round(height, 4))}")
+
+if __name__ == "__main__":
+    main()
